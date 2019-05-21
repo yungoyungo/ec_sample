4376584febc444fa05faa747d38343a580231a07
yungo <29724679+yungoyungo@users.noreply.github.com>
2019-05-21 18:12:47 +0900

    sql追加

---
+++ sql/cart.sql
@@ -0,0 +1,4 @@
+create table cart (
+    product_id INT NOT NULL primary key,
+    quantity INT not null
+);
---
+++ sql/products.sql
@@ -0,0 +1,6 @@
+create table mst_product (
+    id INT NOT NULL AUTO_INCREMENT primary key, 
+    name CHAR(30) NOT NULL, 
+    price INT not null, 
+    image text
+);
